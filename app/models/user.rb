38aa762ca159c56500253f197d0f6fa6440e2c25
larakotnik218 <lara.kotnik@scv.si>
2021-01-24 19:34:42 +0100

    google login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+         :recoverable, :rememberable, :validatable
 
   has_many :jobs
 
 
 end
-
-def self.from_google(email:, full_name:, uid:, avatar_url:)
-    return nil unless email =~ /@mybusiness.com\z/
-    create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
-  end
